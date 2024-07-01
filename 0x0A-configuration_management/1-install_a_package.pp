2b2c3e1e38136a6f6a38e8b08b675136540a8f4a
IamGatheru <davidgatheru490@gmail.com>
2024-07-01 13:58:30 -0700

    Added werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,4 +2,9 @@
 package { 'flask':
   ensure   => '2.1.0' and 'installed',
   provider => 'pip3',
+  #require  => Exec['pip3 install werkzeug'],
+}
+package {'werkzeug':
+  ensure   => '2.1.1' and 'installed',
+  provider => 'pip3',
 }
