788215920a7953f851f142089d94f72a6dc73f3d
IamGatheru <davidgatheru490@gmail.com>
2024-07-03 11:42:34 -0700

    Install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 # Install flask from pip3
 package { 'flask':
-  ensure   => '2.1.0' and 'installed',
+  ensure   => '2.1.0',
   provider => 'pip3',
   #require  => Exec['pip3 install werkzeug'],
 }
 package {'werkzeug':
-  ensure   => '2.1.1' and 'installed',
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
